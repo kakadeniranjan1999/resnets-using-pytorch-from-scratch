00b4ff4eca3704d132f6db2cf0366c7695ec3915
Niranjan-Kakade <kakade.niranjan6599@gmail.com>
2021-12-17 21:54:13 +0530

    fix: [resnets-using-pytorch-from-scratch]: Bug fixes and minor refactoring

--- train_model.py
+++ train_model.py
@@ -32,110 +32,6 @@ def load_model(model_path, num_blocks):
     return test_model
 
 
-def main():
-    process_configs = read_config(file_path='model_configs.yml')
-
-    global best_prec1
-
-    # Check the save_dir exists or not
-    if not os.path.exists(process_configs['save_model']['saved_model_dir']):
-        os.makedirs(process_configs['save_model']['saved_model_dir'])
-
-    normalize = transforms.Normalize(mean=[0.5, 0.5, 0.5],
-                                     std=[0.5, 0.5, 0.5])
-
-    # Tests the trained model is test_model is set True
-    if process_configs['test']['test_model']:
-        loss_func = nn.CrossEntropyLoss().cuda()
-
-        test_set = datasets.CIFAR10(root='data/', train=False,
-                                    transform=transforms.Compose([
-                                        transforms.RandomHorizontalFlip(),
-                                        transforms.RandomCrop(32, 4),
-                                        transforms.ToTensor(),
-                                        normalize]), download=True)
-
-        test_loader = torch.utils.data.DataLoader(dataset=test_set, batch_size=128,
-                                                  shuffle=True,
-                                                  num_workers=2, pin_memory=True)
-
-        resnet_model = load_model(model_path=process_configs['test']['model_path'],
-                                  num_blocks=model_blocks[process_configs['train']['arch_name'].lower()])
-
-        test_or_validate(test_loader, resnet_model, loss_func, process_configs['test']['verbose_display_iter'])
-
-        return
-
-    cifar_dataset = datasets.CIFAR10(root=process_configs['load_data']['save_dir'], train=True,
-                                     transform=transforms.Compose([
-                                         transforms.RandomHorizontalFlip(),
-                                         transforms.RandomCrop(32, 4),
-                                         transforms.ToTensor(),
-                                         normalize]), download=True)
-
-    train_set, valid_set = torch.utils.data.random_split(cifar_dataset, [45000, 5000])
-
-    train_loader = torch.utils.data.DataLoader(dataset=train_set, batch_size=process_configs['train']['batch_size'],
-                                               shuffle=True,
-                                               num_workers=process_configs['load_data']['workers'], pin_memory=True)
-
-    valid_loader = torch.utils.data.DataLoader(dataset=valid_set, batch_size=process_configs['train']['batch_size'],
-                                               shuffle=True,
-                                               num_workers=process_configs['load_data']['workers'], pin_memory=True)
-
-    resnet_model = torch.nn.DataParallel(
-        ResNet(BaseResidualBlock, model_blocks[process_configs['train']['arch_name'].lower()]))
-    resnet_model.cuda()
-    cudnn.benchmark = True
-
-    summary(resnet_model, input_size=(3, 32, 32))
-
-    # define loss function
-    loss_func = nn.CrossEntropyLoss().cuda()
-
-    # define SGD optimizer
-    sgd_optimizer = torch.optim.SGD(resnet_model.parameters(), process_configs['train']['learning_rate'],
-                                    momentum=process_configs['train']['momentum'],
-                                    weight_decay=process_configs['train']['weight_decay'],
-                                    )
-
-    # define learning rate scheduler
-    lr_tuner = torch.optim.lr_scheduler.MultiStepLR(sgd_optimizer,
-                                                    milestones=process_configs['train']['lr_scheduler_milestones'],
-                                                    )
-
-    if process_configs['train']['arch_name'] in ['resnet1202', 'resnet110']:
-        for param_group in sgd_optimizer.param_groups:
-            param_group['lr'] = process_configs['train']['learning_rate'] * 0.1
-
-    for epoch in range(0, process_configs['train']['epochs']):
-
-        print('current lr {:.5e}'.format(sgd_optimizer.param_groups[0]['lr']))
-        train(train_loader, resnet_model, loss_func, sgd_optimizer, epoch,
-              process_configs['train']['verbose_display_iter'])
-        lr_tuner.step()
-
-        # validate the current training progress
-        prec1 = test_or_validate(valid_loader, resnet_model, loss_func,
-                                 process_configs['validate']['verbose_display_iter'])
-
-        # remember best prec@1 and save checkpoint
-        is_best = prec1 > best_prec1
-        best_prec1 = max(prec1, best_prec1)
-
-        if epoch > 0 and epoch % process_configs['save_model']['save_checkpoint_epoch'] == 0:
-            save_checkpoint({
-                'epoch': epoch + 1,
-                'state_dict': resnet_model.state_dict(),
-                'best_prec1': best_prec1,
-            }, is_best, filename=os.path.join(process_configs['save_model']['saved_model_dir'], 'checkpoint.th'))
-
-        save_checkpoint({
-            'state_dict': resnet_model.state_dict(),
-            'best_prec1': best_prec1,
-        }, is_best, filename=os.path.join(process_configs['save_model']['saved_model_dir'], 'model.th'))
-
-
 def train(train_loader, model, loss_func, optimizer, epoch, verbose_display_iter):
     """
         Run one train epoch
@@ -274,6 +170,110 @@ def accuracy(output, target, topk=(1,)):
         correct_k = correct[:k].view(-1).float().sum(0)
         res.append(correct_k.mul_(100.0 / batch_size))
     return res
+
+
+def main():
+    process_configs = read_config(file_path='model_configs.yml')
+
+    global best_prec1
+
+    # Check the save_dir exists or not
+    if not os.path.exists(process_configs['save_model']['saved_model_dir']):
+        os.makedirs(process_configs['save_model']['saved_model_dir'])
+
+    normalize = transforms.Normalize(mean=[0.5, 0.5, 0.5],
+                                     std=[0.5, 0.5, 0.5])
+
+    # Tests the trained model is test_model is set True
+    if process_configs['test']['test_model']:
+        loss_func = nn.CrossEntropyLoss().cuda()
+
+        test_set = datasets.CIFAR10(root='data/', train=False,
+                                    transform=transforms.Compose([
+                                        transforms.RandomHorizontalFlip(),
+                                        transforms.RandomCrop(32, 4),
+                                        transforms.ToTensor(),
+                                        normalize]), download=True)
+
+        test_loader = torch.utils.data.DataLoader(dataset=test_set, batch_size=128,
+                                                  shuffle=True,
+                                                  num_workers=2, pin_memory=True)
+
+        resnet_model = load_model(model_path=process_configs['test']['model_path'],
+                                  num_blocks=model_blocks[process_configs['test']['arch_name'].lower()])
+
+        test_or_validate(test_loader, resnet_model, loss_func, process_configs['test']['verbose_display_iter'])
+
+        return
+
+    cifar_dataset = datasets.CIFAR10(root=process_configs['load_data']['save_dir'], train=True,
+                                     transform=transforms.Compose([
+                                         transforms.RandomHorizontalFlip(),
+                                         transforms.RandomCrop(32, 4),
+                                         transforms.ToTensor(),
+                                         normalize]), download=True)
+
+    train_set, valid_set = torch.utils.data.random_split(cifar_dataset, [45000, 5000])
+
+    train_loader = torch.utils.data.DataLoader(dataset=train_set, batch_size=process_configs['train']['batch_size'],
+                                               shuffle=True,
+                                               num_workers=process_configs['load_data']['workers'], pin_memory=True)
+
+    valid_loader = torch.utils.data.DataLoader(dataset=valid_set, batch_size=process_configs['train']['batch_size'],
+                                               shuffle=True,
+                                               num_workers=process_configs['load_data']['workers'], pin_memory=True)
+
+    resnet_model = torch.nn.DataParallel(
+        ResNet(BaseResidualBlock, model_blocks[process_configs['train']['arch_name'].lower()]))
+    resnet_model.cuda()
+    cudnn.benchmark = True
+
+    summary(resnet_model, input_size=(3, 32, 32))
+
+    # define loss function
+    loss_func = nn.CrossEntropyLoss().cuda()
+
+    # define SGD optimizer
+    sgd_optimizer = torch.optim.SGD(resnet_model.parameters(), process_configs['train']['learning_rate'],
+                                    momentum=process_configs['train']['momentum'],
+                                    weight_decay=process_configs['train']['weight_decay'],
+                                    )
+
+    # define learning rate scheduler
+    lr_tuner = torch.optim.lr_scheduler.MultiStepLR(sgd_optimizer,
+                                                    milestones=process_configs['train']['lr_scheduler_milestones'],
+                                                    )
+
+    if process_configs['train']['arch_name'] in ['resnet1202', 'resnet110']:
+        for param_group in sgd_optimizer.param_groups:
+            param_group['lr'] = process_configs['train']['learning_rate'] * 0.1
+
+    for epoch in range(0, process_configs['train']['epochs']):
+
+        print('current lr {:.5e}'.format(sgd_optimizer.param_groups[0]['lr']))
+        train(train_loader, resnet_model, loss_func, sgd_optimizer, epoch,
+              process_configs['train']['verbose_display_iter'])
+        lr_tuner.step()
+
+        # validate the current training progress
+        prec1 = test_or_validate(valid_loader, resnet_model, loss_func,
+                                 process_configs['validate']['verbose_display_iter'])
+
+        # remember best prec@1 and save checkpoint
+        is_best = prec1 > best_prec1
+        best_prec1 = max(prec1, best_prec1)
+
+        if epoch > 0 and epoch % process_configs['save_model']['save_checkpoint_epoch'] == 0:
+            save_checkpoint({
+                'epoch': epoch + 1,
+                'state_dict': resnet_model.state_dict(),
+                'best_prec1': best_prec1,
+            }, is_best, filename=os.path.join(process_configs['save_model']['saved_model_dir'], 'checkpoint.th'))
+
+        save_checkpoint({
+            'state_dict': resnet_model.state_dict(),
+            'best_prec1': best_prec1,
+        }, is_best, filename=os.path.join(process_configs['save_model']['saved_model_dir'], 'model.th'))
 
 
 if __name__ == '__main__':
